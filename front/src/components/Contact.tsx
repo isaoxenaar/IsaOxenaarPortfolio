9bda1bee0203f4d75ab5f4b17d5b5757b4fb3eaa
isaoxenaar <isamoxenaar@gmail.com>
2022-06-17 21:44:29 +0200

    fullt responsive wink on hover

--- front/src/components/Contact.tsx
+++ front/src/components/Contact.tsx
@@ -8,8 +8,8 @@ const Contact = () => {
     <text className="Contact--intro">For inquiries Contact <a className="Contact--salt" href="https://salt.dev/">{salt}</a>directly <h2>salt@dev.se</h2> or get in touch with me personally <h2>isa.oxenaar@appliedtechnology.se</h2> </text>
     </article>
     <section className="Contact--links">
-    <a>linkedin</a>
-    <a>github</a>
+    {/* <a>linkedin</a>
+    <a>github</a> */}
     </section>
  </section>)
 }
--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -8,8 +8,11 @@ const Home = () => {
  return (<section className="Home--main">
      <Whale/>
      <h1 className="Home--text">Hej! I am Full Stack .NET & Full Stack Javascript developer. Currently as a consultant at <a className="Home--link" href="https://salt.dev/">Salt</a> in Oslo.</h1>
+     <section className="Home--drawing">
      <img className="Home--img" alt="" src={whale} />
      <div className="Home--wink"></div>
+
+     </section>
  </section>)
 }
 
